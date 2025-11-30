59c65a262bf9fe6790d2b631611f97449f9b64d9
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2025-11-30 10:43:28 +0300

    fixes

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const userName = document.getElementById("username").value.trim();
-const email = document.getElementById("email").value.trim();
-const password = document.getElementById("password").value.trim();
-const form = document.getElementById('registration-form"');
-const formFeedback = document.getElementById("form-feedback");
-let isValid = true;
-let message = [];
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (userName.length < 3) {
-    isValid = false;
-    message.push("username's length must be longer than 3");
-  }
-  if (!email.includes("@") && !email.includes(".")) {
-    isValid = false;
-    message.push("email must be valid");
-  }
-  if (password.length < 8) {
-    isValid = false;
-    message.push("password's length must be longer than 8");
-  }
-  formFeedback.style.display = "block";
-  if (isValid) {
-    formFeedback.innerText = "Registration successful!";
-    formFeedback.style.color = "#28a745";
-  } else {
-    formFeedback.innerHTML = message.join("<br>");
-    formFeedback.style.color = "#dc3545";
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const userName = document.getElementById("username").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const form = document.getElementById('registration-form"');
+  const formFeedback = document.getElementById("form-feedback");
+  let isValid = true;
+  let message = [];
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (userName.length < 3) {
+      isValid = false;
+      message.push("username's length must be longer than 3");
+    }
+    if (!email.includes("@") && !email.includes(".")) {
+      isValid = false;
+      message.push("email must be valid");
+    }
+    if (password.length < 8) {
+      isValid = false;
+      message.push("password's length must be longer than 8");
+    }
+    formFeedback.style.display = "block";
+    if (isValid) {
+      formFeedback.innerText = "Registration successful!";
+      formFeedback.style.color = "#28a745";
+    } else {
+      formFeedback.innerHTML = message.join("<br>");
+      formFeedback.style.color = "#dc3545";
+    }
+  });
 });
